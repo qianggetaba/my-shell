ce5195bfc58cd32f05afcd261fa764da79252a51
qinaggetaba <hui740024906@163.com>
2020-05-17 19:45:45 +0800

    add

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # my-shell
 shell utils
 
+工作，学习时掌握的shell奇技淫巧 -- 【水滴石穿,绳锯木断】
+
+
+
 
 ### 使用shell脚本mysql binlog 转sql语句
 [binlog2sql.sh](binlog2sql.sh)
@@ -11,3 +15,14 @@ shell utils
 ### centos开启X11转发，使用XManager实现gui安装linux软件
 [misc.sh#L6](misc.sh#L6)
 
+### 使用网络快速获取与设置正确的系统时间
+[misc.sh#L13](misc.sh#L13)
+
+### 公钥私钥配对检查，没有输出就是正确的配对
+[misc.sh#L18](misc.sh#L18)
+
+### 通过私钥生成公钥
+[misc.sh#L23](misc.sh#L23)
+
+### fdisk无交互命令行分区，虽然可以用parted命令，但是好像与fdisk的分区有区别
+[misc.sh#L26](misc.sh#L26)
--- misc.sh
+++ misc.sh
@@ -8,3 +8,27 @@ service sshd restart
 # 重新连接ssh
 xclock # 测试是否显示
 echo 'export DISPLAY=192.168.199.188:0.0' >> /home/oracle/.bash_profile # 可能需要设置display
+
+
+date -d "$(curl -sI baidu.com| grep -i '^date:'|cut -d' ' -f2-)"
+date -d "$(curl -sI baidu.com| grep -i '^date:'|cut -d' ' -f2-)" | xargs -I {} date --date='{}' "+%Y-%m-%d %H:%M:%S"
+date -s "$(date -d "$(curl -sI baidu.com| grep -i '^date:'|cut -d' ' -f2-)" | xargs -I {} date --date='{}' "+%Y-%m-%d %H:%M:%S")"
+
+
+PRIVKEY=mykey
+TESTKEY=mykey.pub
+diff <( ssh-keygen -y -e -f "$PRIVKEY" ) <( ssh-keygen -y -e -f "$TESTKEY" )
+
+
+ssh-keygen -y -f id_rsa > id_rsa.pub
+
+
+sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk /dev/sda
+  o # clear the in memory partition table
+  n # new partition
+  p # primary partition
+  1 # partition number 1
+    # default - start at beginning of disk 
+    #
+  w
+EOF
